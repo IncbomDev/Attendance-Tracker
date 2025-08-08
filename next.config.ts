95b91a276e01fa66a00eb96ac209aeaf0adfbb4c
nathan <incbom.dev@gmail.com>
2025-08-08 19:03:34 -0400

    remove cloudflare

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,3 @@ const nextConfig: NextConfig = withPWA({
 });
 
 export default nextConfig;
-
-// added by create cloudflare to enable calling `getCloudflareContext()` in `next dev`
-import { initOpenNextCloudflareForDev } from '@opennextjs/cloudflare';
-initOpenNextCloudflareForDev();
